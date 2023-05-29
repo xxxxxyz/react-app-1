5073b7949a7872967d47f132fe8f9863988bc2f9
yuzexia <xiayuze0406@gmail.com>
2023-05-29 21:10:35 +1000

    Initialize what is GPT3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React from "react";
+
 import {
-  Header,
-  Blog,
-  Features,
-  Possibility,
-  WhatGPT,
   Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
 } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 
 import "./App.css";
 
-import { CTA, Brand, Navbar } from "./components";
-
-const App = () => {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-};
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
